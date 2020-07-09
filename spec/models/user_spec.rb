d676b5924057ea61c4a5c5c14ce5fd0ea75139aa
Tad Preston <tad.preston@gatewaystaff.com>
2020-07-09 15:40:09 -0500

    added some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(email: "test@test.com") }
+  describe ".hello" do
+    it "says hello with the email" do
+      expect(subject.hello).to eq("Hello test@test.com")
+    end
+  end
 end
